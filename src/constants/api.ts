b80aa8bdc09952aaee89316b1eb622c8327c1cc7
DanSoW <67583913+DanSoW@users.noreply.github.com>
2023-12-03 18:13:13 +0800

    fix: Исправил маршруты

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,12 +1,12 @@
 const Api = {
-    MAIN_SERVER: "http://localhost:5000/api",
-    SIGN_IN: "/auth/sign-in",
-    SIGN_UP: "/auth/sign-up",
-    REFRESH_TOKEN: "/auth/refresh/token",
-    DOOR_GET_ALL: "/user/door/get/all",
-    MAILER_COMMON_SEND: "/user/mailer/common/send",
-    MAILER_ORDER_SEND: "/user/mailer/order/send",
-    FILTER_INFO: "/user/filter/info"
+    MAIN_SERVER: "http://localhost:5000",
+    SIGN_IN: "/api/auth/sign-in",
+    SIGN_UP: "/api/auth/sign-up",
+    REFRESH_TOKEN: "/api/auth/refresh/token",
+    DOOR_GET_ALL: "/api/user/door/get/all",
+    MAILER_COMMON_SEND: "/api/user/mailer/common/send",
+    MAILER_ORDER_SEND: "/api/user/mailer/order/send",
+    FILTER_INFO: "/api/user/filter/info"
 };
 
 export default Api;
