4335dd56eb4e7c4916f7e0994d61f02d874bb522
DanSoW <67583913+DanSoW@users.noreply.github.com>
2023-12-03 18:11:47 +0800

    fix: Исправил API

--- src/constants/admin-api.ts
+++ src/constants/admin-api.ts
@@ -1,17 +1,17 @@
 const AdminApi = {
-  DOOR_GET_ALL: "/api/admin/door/get/all",
-  DOOR_ADD: "/api/admin/door/add",
-  DOOR_IMAGES_EDIT: "/api/admin/door/images/edit",
-  DOOR_INFO_EDIT: "/api/admin/door/info/edit",
-  DOOR_DELETE: "/api/admin/door/delete",
-  DOOR_CHARACTERISTIC_ADD: "/api/admin/door/characteristic/add",
-  DOOR_CHARACTERISTIC_INFO_EDIT: "/api/admin/door/characteristic/info/edit",
-  DOOR_CHARACTERISTIC_IMAGE_ADD: "/api/admin/door/characteristic/image/add",
-  DOOR_CHARACTERISTIC_IMAGE_DELETE: "/api/admin/door/characteristic/image/delete",
-  DOOR_CHARACTERISTIC_DELETE: "/api/admin/door/characteristic/delete",
-  DOOR_IMAGE_ENTRY_EDIT: "/api/admin/door/image-entry/edit",
-  DOOR_IMAGE_EXIT_EDIT: "/api/admin/door/image-exit/edit",
-  FILTER_INFO_ADD: "/api/admin/filter/info/add"
+  DOOR_GET_ALL: "/admin/door/get/all",
+  DOOR_ADD: "/admin/door/add",
+  DOOR_IMAGES_EDIT: "/admin/door/images/edit",
+  DOOR_INFO_EDIT: "/admin/door/info/edit",
+  DOOR_DELETE: "/admin/door/delete",
+  DOOR_CHARACTERISTIC_ADD: "/admin/door/characteristic/add",
+  DOOR_CHARACTERISTIC_INFO_EDIT: "/admin/door/characteristic/info/edit",
+  DOOR_CHARACTERISTIC_IMAGE_ADD: "/admin/door/characteristic/image/add",
+  DOOR_CHARACTERISTIC_IMAGE_DELETE: "/admin/door/characteristic/image/delete",
+  DOOR_CHARACTERISTIC_DELETE: "/admin/door/characteristic/delete",
+  DOOR_IMAGE_ENTRY_EDIT: "/admin/door/image-entry/edit",
+  DOOR_IMAGE_EXIT_EDIT: "/admin/door/image-exit/edit",
+  FILTER_INFO_ADD: "/admin/filter/info/add"
 };
 
 export default AdminApi;
